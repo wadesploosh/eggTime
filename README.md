eb455a03138b812f9e1cf8c6982348174c100c75
wadeSploosh <wade@sploosh.com>
2015-06-22 11:24:38 +0200

    A few tweaks

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 eggTime is a fun personal project that makes use of HTML, CSS, JavaScript & audio to determine how long your eggs need to boil for based on how you like em done!
 
 I’ve wrapped it using Phonegap to access the devices vibration feature.
+
+More to come...
--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -21,6 +21,34 @@ module.exports = [
         "clobbers": [
             "navigator.splashscreen"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -28,7 +56,8 @@ module.exports.metadata =
 {
     "org.apache.cordova.vibration": "0.3.13",
     "org.apache.cordova.statusbar": "0.1.10",
-    "org.apache.cordova.splashscreen": "1.0.0"
+    "org.apache.cordova.splashscreen": "1.0.0",
+    "org.apache.cordova.geolocation": "0.3.12"
 }
 // BOTTOM OF METADATA
 });
